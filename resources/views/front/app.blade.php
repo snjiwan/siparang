32fca208bc8a10ff8e98ff54b65fce2197a23dcb
snjiwan <143898514+snjiwan@users.noreply.github.com>
2024-08-16 20:26:28 +0700

    login regist

--- resources/views/front/app.blade.php
+++ resources/views/front/app.blade.php
@@ -33,7 +33,7 @@
 								<li><a href="index.html">Home</a></li>
 								<li><a href="generic.html">Generic</a></li>
 								<li><a href="elements.html">Elements</a></li>
-								<li><a href="#">Log In</a></li>
+								<li><a href="{{ route('login') }}">Log In</a></li>
 								<li><a href="#">Sign Up</a></li>
 							</ul>
 							<a href="#" class="close">Close</a>
