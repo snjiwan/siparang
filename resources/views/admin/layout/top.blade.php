8eb9328fa5878e735fbe0b2b6353152de2cd89ce
Naufal Rizqi Ramadhan <214060012.mhs@stmikjabar.ac.id>
2024-08-17 16:44:24 +0700

    Menambahkan button action

--- resources/views/admin/layout/top.blade.php
+++ resources/views/admin/layout/top.blade.php
@@ -11,6 +11,8 @@
   <link rel="stylesheet" href="{{asset('admin/plugins/fontawesome-free/css/all.min.css')}}">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
   <link rel="stylesheet" href="{{asset('admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
   <!-- iCheck -->
