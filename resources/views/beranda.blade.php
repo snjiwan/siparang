1de11a74832bf3cb2c053daa169b43b929a47ef6
snjiwan <143898514+snjiwan@users.noreply.github.com>
2024-08-20 10:46:26 +0700

    fix ui 1

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -7,7 +7,7 @@
       <div class="container-fluid">
         <nav class="navbar navbar-expand-lg custom_nav-container">
           <a class="navbar-brand" href="index.html">
-            <img src="{{asset('front/images/logo.png')}}" alt="" />
+            <img src="{{asset('front/images/logo_fix.png')}}" alt="" />
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,19 +17,10 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav  ">
               <li class="nav-item active">
-                <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="{{url ('/')}}">Beranda <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="about.html"> About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="shop.html">Shop </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="furniture.html"> Furniture </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="contact.html">Contact us</a>
+                <a class="nav-link" href="#about">Kontak</a>
               </li>
             </ul>
             <div class="user_option">
@@ -40,7 +31,7 @@
                 </span>
               </a>
               <form class="form-inline my-2 my-lg-0 ml-0 ml-lg-4 mb-3 mb-lg-0">
-                <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
+                <!-- <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button> -->
               </form>
             </div>
           </div>
@@ -541,49 +532,83 @@
     <div class="container">
       <div class="heading_container">
         <h2>
-          Kegiatan - Kegiatan
+          Testimonial
         </h2>
       </div>
     </div>
 
     <div class="container">
       <div class="client_container layout_padding2">
-        @foreach ($kegiatan as $k)
+        @foreach($kegiatan as $k)
         <div class="client_box b-1">
           <div class="client-id">
+            <div class="img-box">
+              <img src="{{asset('images/kegiatan/'.$k->gambar)}}" alt="" />
+            </div>
             <div class="name">
               <h5>
-                {{$k -> nama_kegiatan}}
+                Magna
               </h5>
+              <p>
+                
+              </p>
             </div>
           </div>
           <div class="detail">
             <p>
-{{$k -> deskripsi_kegiatan}}
+            {{$k->deskripsi_kegiatan}}
             </p>
             <div>
-            <a href="{{url ('/activity/'.$k->id)}}"><div class="arrow_img"></div></a>  
-                
-              </div>
+             <a href="{{ url('/activity/'. $k->id) }}"><div class="arrow_img"></div></a> 
+              
             </div>
           </div>
-          @endforeach
-        </section>
+        </div>
+        @endforeach
+        <!-- <div class="client_box b-2">
+          <div class="client-id">
+            <div class="img-box">
+              <img src="images/client-2.png" alt="" />
+            </div>
+            <div class="name">
+              <h5>
+                Aliqua
+              </h5>
+              <p>
+                Consectetur adipiscing
+              </p>
+
+            </div>
+          </div>
+          <div class="detail">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrudLorem ipsum
+            </p>
+            <div>
+              <div class="arrow_img">
+              </div> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
 
   <section class="contact_section layout_padding">
     <div class="container ">
       <div class="heading_container">
         <h2 class="">
-          Contact Us
+          Lokasi Pariwisata
         </h2>
       </div>
 
     </div>
     <div class="container">
       <div class="row">
-        <div class="col-md-6">
-          <form action="">
+        <!-- <div class="col-md-6"> -->
+          <!-- <form action="">
             <div>
               <input type="text" placeholder="Name" />
             </div>
@@ -601,12 +626,12 @@
                 SEND
               </button>
             </div>
-          </form>
-        </div>
-        <div class="col-md-6">
+          </form> -->
+        <!-- </div> -->
+        <div class="col-md-12">
           <div class="map_container">
             <div class="map-responsive">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31678.058857956483!2d107.87956019218839!3d-7.037778149696546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c8fed4167b61%3A0x871028ee1d33b7b2!2sCiherang%2C%20Kec.%20Nagreg%2C%20Kabupaten%20Bandung%2C%20Jawa%20Barat!5e0!3m2!1sid!2sid!4v1724074843305!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31678.058857956483!2d107.87956019218839!3d-7.037778149696546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c8fed4167b61%3A0x871028ee1d33b7b2!2sCiherang%2C%20Kec.%20Nagreg%2C%20Kabupaten%20Bandung%2C%20Jawa%20Barat!5e0!3m2!1sid!2sid!4v1724074843305!5m2!1sid!2sid" width="1100" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
           </div>
         </div>
@@ -617,11 +642,11 @@
   <!-- end client section -->
 
   <!-- info section -->
-  <section class="info_section layout_padding2">
+  <section id="about" class="info_section layout_padding2">
     <div class="container">
       <div class="info_logo">
         <h2>
-          Digitf
+          Wisata Ciherang
         </h2>
       </div>
       <div class="row">
@@ -629,19 +654,19 @@
         <div class="col-md-3">
           <div class="info_contact">
             <h5>
-              About Shop
+              Tentang Wisata
             </h5>
             <div>
               <div class="img-box">
-                <img src="images/location-white.png" width="18px" alt="">
+                <img src="{{asset('front/images/location-white.png')}}" width="18px" alt="">
               </div>
               <p>
-                Address
+              Ciherang Kec. Nagreg Kabupaten Bandung Jawa Barat
               </p>
             </div>
             <div>
               <div class="img-box">
-                <img src="images/telephone-white.png" width="12px" alt="">
+                <img src="{{asset('front/images/telephone-white.png')}}" width="12px" alt="">
               </div>
               <p>
                 +01 1234567890
@@ -649,10 +674,10 @@
             </div>
             <div>
               <div class="img-box">
-                <img src="images/envelope-white.png" width="18px" alt="">
+                <img src="{{asset('front/images/envelope-white.png')}}" width="18px" alt="">
               </div>
               <p>
-                demo@gmail.com
+                Ciherang@gmail.com
               </p>
             </div>
           </div>
@@ -663,13 +688,13 @@
               Informations
             </h5>
             <p>
-              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+              Saat ini beberapa tempat wisata sedang dalam proses persiapan.
             </p>
           </div>
         </div>
 
-        <div class="col-md-3">
-          <div class="info_insta">
+        <!-- <div class="col-md-3"> -->
+          <!-- <div class="info_insta">
             <h5>
               Instagram
             </h5>
@@ -677,12 +702,12 @@
               <div>
                 <a href="">
                   <div class="insta-box b-1">
-                    <img src="images/i-1.jpg" alt="">
+                    <img src="{{asset('front/images/i-1.jpg')}}" alt="">
                   </div>
                 </a>
                 <a href="">
                   <div class="insta-box b-2">
-                    <img src="images/i-2.jpg" alt="">
+                    <img src="{{asset('front/images/i-2.jpg')}}" alt="">
                   </div>
                 </a>
               </div>
@@ -690,32 +715,32 @@
               <div>
                 <a href="">
                   <div class="insta-box b-3">
-                    <img src="images/i-3.jpg" alt="">
+                    <img src="{{asset('front/images/i-3.jpg')}}" alt="">
                   </div>
                 </a>
                 <a href="">
                   <div class="insta-box b-4">
-                    <img src="images/i-4.jpg" alt="">
+                    <img src="{{asset('front/images/i-4.jpg')}}" alt="">
                   </div>
                 </a>
               </div>
               <div>
                 <a href="">
                   <div class="insta-box b-3">
-                    <img src="images/i-5.jpg" alt="">
+                    <img src="{{asset('front/images/i-5.jpg')}}" alt="">
                   </div>
                 </a>
                 <a href="">
                   <div class="insta-box b-4">
-                    <img src="images/i-6.jpg" alt="">
+                    <img src="{{asset('front/images/i-6.jpg')}}" alt="">
                   </div>
                 </a>
               </div>
             </div>
           </div>
-        </div>
-        <div class="col-md-3">
-          <div class="info_form ">
+        </div> -->
+        <!-- <div class="col-md-3"> -->
+          <!-- <div class="info_form ">
             <h5>
               Newsletter
             </h5>
@@ -724,25 +749,57 @@
               <button>
                 Subscribe
               </button>
-            </form>
-            <div class="social_box">
+            </form> -->
+            <div class="col-md-3">
+          <div class="info_contact">
+            <h5>
+              Sosial Media
+            </h5>
+            <div>
+              <div class="img-box">
+                <img src="{{asset('front/images/fb.png')}}" width="18px" alt="">
+              </div>
+              <p>
+                Ciherang
+              </p>
+            </div>
+            <div>
+              <div class="img-box">
+                <img src="{{asset('front/images/youtube.png')}}" width="12px" alt="">
+              </div>
+              <p>
+                ciherang
+              </p>
+            </div>
+            <div>
+              <div class="img-box">
+                <img src="{{asset('front/images/twitter.png')}}" width="18px" alt="">
+              </div>
+              <p>
+                ciherang
+              </p>
+            </div>
+          </div>
+        </div>
+
+            <!-- <div class="social_box">
               <a href="">
-                <img src="images/fb.png" alt="">
+                <img src="{{asset('front/images/fb.png')}}" alt="">
               </a>
               <a href="">
-                <img src="images/twitter.png" alt="">
+                <img src="{{asset('front/images/twitter.png')}}" alt="">
               </a>
               <a href="">
-                <img src="images/linkedin.png" alt="">
+                <img src="{{asset('front/images/linkedin.png')}}" alt="">
               </a>
               <a href="">
-                <img src="images/youtube.png" alt="">
+                <img src="{{asset('front/images/youtube.png')}}" alt="">
               </a>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
   </section>
 
   <!-- end info_section -->
